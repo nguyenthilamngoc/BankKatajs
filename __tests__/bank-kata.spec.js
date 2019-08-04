00bfa9864ef54460e5c9e04375ebc24c24ddc7d2
nguyenthilamngoc <nguyenlamngoc1361990@yahoo.com>
2019-08-04 18:45:40 +0200

    add new interfaces for deposit and withdraw in API

--- __tests__/bank-kata.spec.js
+++ __tests__/bank-kata.spec.js
@@ -1,37 +1,88 @@
 // @flow
 
 import type { Transaction, Account } from '../bank-kata';
-import { makeTransaction, printStatement } from '../bank-kata';
+// import * as BankKata from '../bank-kata';
+import BankKata from '../bank-kata';
 
 describe('Bank kata', () => {
-    it('should make a deposit on the account', () => {
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
+
+    it('should make a deposit transaction on the account', () => {
         //given
         const depositTr: Transaction = { amount: 1000, date: '03/08/2019', type: 'DEPOSIT' }
         const account: Account = { listTransactions: [] };
 
         //when
-        const updatedAccount = makeTransaction(account, depositTr);
+        const updatedAccount = BankKata.makeTransaction(account, depositTr);
 
         //then
         expect(updatedAccount.listTransactions).toHaveLength(1);
         expect(updatedAccount.listTransactions[0]).toEqual(depositTr);
     });
 
-    it('should make a withdrawal from the account', () => {
+    it('should make a deposit on the account', () => {
+        //given
+        const depositAmount = 1000;
+        const today = '03/08/2019';
+        jest.spyOn(BankKata, 'dateToString').mockReturnValue(today);
+        const account: Account = { listTransactions: [] };
+
+        //when
+        const updatedAccount = BankKata.deposit(account, depositAmount);
+
+        //then
+        expect(updatedAccount.listTransactions).toHaveLength(1);
+        expect(updatedAccount.listTransactions[0].amount).toEqual(depositAmount);
+        expect(updatedAccount.listTransactions[0].date).toEqual(today);
+        expect(updatedAccount.listTransactions[0].type).toEqual('DEPOSIT');
+    });
+
+    it('should make a withdrawal transaction from the account', () => {
         //given
         const withdrawalTr: Transaction = { amount: 100, date: '03/08/2019', type: 'WITHDRAWAL' }
         const depositTr: Transaction = { amount: 1000, date: '03/08/2019', type: 'DEPOSIT' }
         const account: Account = { listTransactions: [depositTr] };
 
         //when
-        const updatedAccount = makeTransaction(account, withdrawalTr);
+        const updatedAccount = BankKata.makeTransaction(account, withdrawalTr);
 
         //then
         expect(updatedAccount.listTransactions).toHaveLength(2);
         expect(updatedAccount.listTransactions[1]).toEqual(withdrawalTr);
     });
 
-    it('should print the history of account', () => {
+    it('should make a withdrawal from the account', () => {
+        //given
+        const withdrawalAmount = 100;
+        const today = '03/08/2019';
+
+        jest.spyOn(BankKata, 'dateToString').mockReturnValue(today);
+        const depositTr: Transaction = { amount: 1000, date: today, type: 'DEPOSIT' }
+        const account: Account = { listTransactions: [depositTr] };
+
+        //when
+        const updatedAccount = BankKata.withdraw(account, withdrawalAmount);
+
+        //then
+        expect(updatedAccount.listTransactions).toHaveLength(2);
+        expect(updatedAccount.listTransactions[1].amount).toEqual(withdrawalAmount);
+        expect(updatedAccount.listTransactions[1].date).toEqual(today);
+        expect(updatedAccount.listTransactions[1].type).toEqual('WITHDRAWAL');
+
+    });
+
+    it('should return date format dd/MM/yyy', () => {
+        //given
+        const today = new Date('2019-08-03T03:24:00');
+        //when
+        const todayStr = BankKata.dateToString(today);
+        //then
+        expect(todayStr).toEqual('03/08/2019');
+    });
+
+    it('should return the history of account', () => {
         //given
         const withdrawalTr3: Transaction = { amount: 200, date: '04/08/2019', type: 'WITHDRAWAL' }
         const withdrawalTr2: Transaction = { amount: 100, date: '03/08/2019', type: 'WITHDRAWAL' }
@@ -39,7 +90,7 @@ describe('Bank kata', () => {
         const account: Account = { listTransactions: [depositTr1, withdrawalTr2, withdrawalTr3] };
 
         //when
-        const linesToPrint = printStatement(account);
+        const linesToPrint = BankKata.printStatement(account);
 
         //then
         expect(linesToPrint[0]).toEqual('DATE       | AMOUNT  | BALANCE');
--- bank-kata.js
+++ bank-kata.js
@@ -9,17 +9,34 @@ export type Account = {
     listTransactions: Transaction[]
 }
 
-export function makeTransaction(account: Account, transaction: Transaction): Account {
+/******* Utils ********/
+function makeTransaction(account: Account, transaction: Transaction): Account {
     account.listTransactions.push(transaction);
     return account;
 }
 
-export function printStatement(account) {
-    let linesToPrint: string[] = [];
-    const firstLine = 'DATE       | AMOUNT  | BALANCE';
+function dateToString(date) {
+    const day = ('0' + date.getDate()).slice(-2);
+    const month = ('0' + (date.getMonth() + 1)).slice(-2);
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+}
 
-    let balance = 0;
-    const lines = account.listTransactions.map((transaction: Transaction) => {
+/******* API ******/
+function deposit(account: Account, amount: number) {
+    const transaction: Transaction = { amount, date: exportFunctions.dateToString(new Date()), type: 'DEPOSIT' };
+    return exportFunctions.makeTransaction(account, transaction);
+}
+
+function withdraw(account: Account, amount: number) {
+    const transaction: Transaction = { amount, date: exportFunctions.dateToString(new Date()), type: 'WITHDRAWAL' };
+    return exportFunctions.makeTransaction(account, transaction);
+}
+
+function printStatement(account: Account): string[] {
+    const firstLine = 'DATE       | AMOUNT  | BALANCE';
+    let balance: number = 0;
+    const statementLines: string[] = account.listTransactions.map((transaction: Transaction) => {
         const amount = transaction.type === 'DEPOSIT' ? transaction.amount : -transaction.amount;
         balance += amount;
         return transaction.date
@@ -28,6 +45,15 @@ export function printStatement(account) {
             + " | "
             + balance.toFixed(2)
     }).reverse();
-    linesToPrint = [firstLine, ...lines];
-    return linesToPrint;
+    return [firstLine, ...statementLines];
 }
+
+/******* Export functions *******/
+const exportFunctions = {
+    makeTransaction,
+    dateToString,
+    deposit,
+    withdraw,
+    printStatement
+};
+export default exportFunctions;
